8257dbbb3ca489bf5f096b7377934d451c1ff5ef
SunHi <doan.nhathuy.03@gmail.com>
2023-08-03 22:02:00 +0700

    CRUD for Category

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,93 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> CategoryList = _context.Categories.ToList();
+            return View(CategoryList);
+        }
+
+        [HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
+		[HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+				TempData["success"] = "Category created successfully!";
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+		[HttpGet]
+		public IActionResult Edit(int? id)
+		{
+			if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? category = _context.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+		}
+		[HttpPost]
+		public IActionResult Edit(Category category)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.Categories.Update(category);
+				_context.SaveChanges();
+				TempData["success"] = "Category updated successfully!";
+				return RedirectToAction("Index");
+			}
+			return View(category);
+		}
+
+		[HttpGet]
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			Category? category = _context.Categories.FirstOrDefault(c => c.Id == id);
+			if (category == null)
+			{
+				return NotFound();
+			}
+			return View(category);
+		}
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeletePOST(int? id)
+		{
+			Category? category = _context.Categories.FirstOrDefault(c => c.Id == id);
+			if (category == null)
+			{
+				return NotFound();
+			}
+			_context.Categories.Remove(category);
+			_context.SaveChanges();
+			TempData["success"] = "Category deleted successfully!";
+			return RedirectToAction("Index");
+		}
+	}
+}
