c10ddea0fe350f8282567bfa7ea23f6244fa3b9b
SunHi <doan.nhathuy.03@gmail.com>
2023-08-04 10:55:02 +0700

    Create BulkyWebRazor project with base things

---
+++ BulkyWebRazor_Temp/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BulkyWebRazor_Temp.Models
+{
+	public class Category
+	{
+		public int Id { get; set; }
+
+		[DisplayName("Category Name"), MaxLength(30)]
+		public string Name { get; set; } = null!;
+
+		[DisplayName("Display Order"), Range(1, 100)]
+		public int DisplayOrder { get; set; }
+	}
+}
