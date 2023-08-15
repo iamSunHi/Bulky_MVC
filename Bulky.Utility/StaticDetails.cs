e29aba040694c2e8277af16863d8376db68a5ff7
SunHi <doan.nhathuy.03@gmail.com>
2023-08-15 16:23:10 +0700

    Add Authorization

--- Bulky.Utility/StaticDetails.cs
+++ Bulky.Utility/StaticDetails.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace BulkyBook.Utility
 {
-	internal class StaticDetails
+	public class StaticDetails
 	{
+		public const string Role_Customer = "Customer";
+		public const string Role_Company = "Company";
+		public const string Role_Admin = "Admin";
+		public const string Role_Employee = "Employee";
 	}
 }
--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -1,14 +1,18 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
 using BulkyBook.Models.ViewModels;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
+using System.Data;
 
 namespace BulkyBookWeb.Areas.Admin.Controllers
 {
 	[Area("Admin")]
-	public class ProductController : Controller
+    [Authorize(Roles = StaticDetails.Role_Admin)]
+    public class ProductController : Controller
 	{
 		private readonly IUnitOfWork _unitOfWork;
 		private readonly IWebHostEnvironment _webHostEnvironment;
