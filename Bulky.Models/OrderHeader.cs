c5fbc3ef79018498b07a63ef85f4f214289e62a3
SunHi <doan.nhathuy.03@gmail.com>
2023-08-21 21:50:43 +0700

    Add ShoppingCart and Order model, update database

---
+++ Bulky.Models/OrderHeader.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BulkyBook.Models
+{
+    public class OrderHeader
+    {
+        public int Id { get; set; }
+
+        public string ApplicationUserId { get; set; } = null!;
+        [ValidateNever]
+        public ApplicationUser ApplicationUser { get; set; } = null!;
+
+        public DateTime OrderDate { get; set; }
+        public DateTime ShippingDate { get; set; }
+        public double OrderTotal { get; set; }
+        public string? OrderStatus { get; set; }
+        public string? PaymentStatus { get; set; }
+        public string? TrackingNumber { get; set; }
+        public string? Carrier { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public DateTime PaymentDueDate { get; set; }
+
+        public string? SessionId { get; set; }
+        public string? PaymentIntentId { get; set; }
+
+        public string PhoneNumber { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string State { get; set; } = null!;
+        public string PostalCode { get; set; } = null!;
+        public string Name { get; set; } = null!;
+    }
+}
---
+++ Bulky.Models/ShoppingCart.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BulkyBook.Models
+{
+    public class ShoppingCart
+    {
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+        [ValidateNever]
+        public Product? Product { get; set; }
+        [Range(1, 1000, ErrorMessage = "Please enter a value between 1 and 1000")]
+        public int Count { get; set; }
+
+        public string ApplicationUserId { get; set; } = null!;
+        [ValidateNever]
+        public ApplicationUser ApplicationUser { get; set; } = null!;
+
+        [NotMapped]
+        public double Price { get; set; }
+    }
+}
