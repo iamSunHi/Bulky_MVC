0f32b5b6daad96daaa2623c09ececaeaeae798a1
NhatHuy <doan.nhathuy.03@gmail.com>
2023-09-05 20:27:57 +0700

    Change some UI Bugs

---
+++ Bulky.DataAccess/DbInitializer/DbInitializer.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.DbInitializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        public readonly UserManager<IdentityUser> _userManager;
+
+        public void Initialize()
+        {
+            // migrations if they are not applied
+
+            
+            //create roles if they are not created
+
+
+            // if roles are not created, then we will create admin user as well
+
+        }
+    }
+}
